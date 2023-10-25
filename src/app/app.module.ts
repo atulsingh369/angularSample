20031f660ccaa62186b4337ef08f9eb02c458817
atulsingh369 <businessatul7684@gmail.com>
2023-10-25 18:31:54 +0530

    Found the alernative  of usestate hook of react

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,39 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideDatabase, getDatabase } from '@angular/fire/database';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+
+import { RouterModule } from '@angular/router';
+// import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+// import { MyserviceService } from './myservice.service';
+// import { NewCmpComponent } from './new-cmp/new-cmp.component';
+// import { ChangeTextDirective } from './change-text.directive';
+// import { SqrtPipe } from './app.sqrt';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    RouterModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore()),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
+
+export class formSubmit {
+  constructor() {}
+  handleSubmit(): void {
+    alert('form is submited');
+  }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'angular-dummy-17f14',
+    appId: '1:1019213813227:web:5c1547458f7d57f01375ae',
+    databaseURL: 'https://angular-dummy-17f14-default-rtdb.asia-southeast1.firebasedatabase.app',
+    storageBucket: 'angular-dummy-17f14.appspot.com',
+    apiKey: 'AIzaSyAOJorQe4rY-6YmEBAfxQFbZUFdKjcA7zw',
+    authDomain: 'angular-dummy-17f14.firebaseapp.com',
+    messagingSenderId: '1019213813227',
+    measurementId: 'G-W9R9CYJX11',
+  },
+};
